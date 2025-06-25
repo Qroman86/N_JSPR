a691db0e78271f896b8b711e78b616e90a992145
romanQ <roman.kudr@gmail.com>
2025-06-25 21:44:59 +0300

    add handlers

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,9 +1,41 @@
 package ru.netology;
 
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
         Server server = new Server(64);
-        server.start(9999);
+        server.addHandler("GET", "/messages", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) throws IOException {
+                final var content = "messages content for GET Method".getBytes();
+                responseStream.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + "text/plain" + "\r\n" +
+                                "Content-Length: " + content.length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                responseStream.write(content);
+                responseStream.flush();
+            }
+        });
+        server.addHandler("POST", "/messages", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) throws IOException {
+                final var content = "messages content for POST Method".getBytes();
+                responseStream.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + "text/plain" + "\r\n" +
+                                "Content-Length: " + content.length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                responseStream.write(content);
+                responseStream.flush();
+            }
+        });
+        server.listen(9999);
     }
 }
 
